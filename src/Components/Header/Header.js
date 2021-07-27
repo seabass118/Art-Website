59c616af12249a2b398edbf12c4a51eb4f9ee76a
seabass118 <seb.blackley@protonmail.com>
2021-07-27 17:09:06 +0100

    Home page changes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import './Header.css';
+
 
 function Header() {
     return (
         <div className="Header">
-            <div className="Header_Cont" style={Header_Cont}></div>
+            <div className="Header_Cont" style={Header_Cont}>
+                <div style={{"width": "calc(100% - 20px)", "height": "calc(100% - 20px)", "padding": "10px", "display": "flex", "flex-direction": "row", "align-items": "center"}}>
+                    <div className="logo">
+                        Simon.
+                    </div>
+                    <input id="burger" type="checkbox" />
+
+                    <label for="burger">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </label>
+
+                    <nav>    
+                    <ul style={{"list-style-type": "none"}}>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/Gallery">Gallery</a></li>
+                        <li><a href="/Contact">Contact</a></li>
+                    </ul>  
+                    </nav>
+                </div>
+            </div>
         </div>
     )
 }
+
 
 const Header_Cont = {
     width: '100%',
--- src/Components/Main/Home/Right/ImageSlider/ImageSlider.js
+++ src/Components/Main/Home/Right/ImageSlider/ImageSlider.js
@@ -7,11 +7,26 @@ import sliderArrow from '../../../Images/Icons/slider_arrow.svg';
 import './ImageSlider.css';
 import 'react-slideshow-image/dist/styles.css';
 import { Slide } from 'react-slideshow-image';
+
 const slideImages = [
     simon,
     new_york,
     girl,
     dont_walk
+];
+
+const slideTitles = [
+    'Simon',
+    'New York Cafe Crossing',
+    'Girl',
+    'Dont Walk'
+];
+
+const slideLinks = [
+    'https://www.artfinder.com/artist/simon-blackley#/quick-view/simon-e9b2e',
+    'https://www.artfinder.com/artist/simon-blackley/page-2#/quick-view/cafe-crossing-new-york',
+    'artfinder.com/artist/simon-blackley#/quick-view/girl-2d0a4',
+    'https://www.artfinder.com/artist/simon-blackley/page-2#/quick-view/dont-walk-8a777'
 ];
 
 const Properties = {
@@ -24,24 +39,48 @@ function ImageSlider() {
         <div className="ImageSlider">
             <Slide easing="ease" {...Properties}>
                 <div className="each-slide">
-                    <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
+                    <div className="slide_title">
+                        <div style={slide_text}>
+                            <a className="slideLink" href={slideLinks[0]}>{slideTitles[0]}</a>
+                        </div>
+                    </div>
+                    <div className="slide_image" style={{'backgroundImage': `url(${slideImages[0]})`, 'height': 'calc(100vh - 50px)'}}>
                     </div>
                 </div>
                 <div className="each-slide">
-                    <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+                    <div className="slide_title">
+                        <div style={slide_text}>
+                            <a className="slideLink" href={slideLinks[1]}>{slideTitles[1]}</a>
+                        </div>
+                    </div>
+                    <div className="slide_image" style={{'backgroundImage': `url(${slideImages[1]})`, 'height': 'calc(100vh - 50px)'}}>
                     </div>
                 </div>
                 <div className="each-slide">
-                    <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+                    <div className="slide_title">
+                        <div style={slide_text}>
+                            <a className="slideLink" href={slideLinks[2]}>{slideTitles[2]}</a>
+                        </div>
+                    </div>
+                    <div className="slide_image" style={{'backgroundImage': `url(${slideImages[2]})`, 'height': 'calc(100vh - 50px)'}}>
                     </div>
                 </div>
                 <div className="each-slide">
-                    <div style={{'backgroundImage': `url(${slideImages[3]})`}}>
+                    <div className="slide_title">
+                        <div style={slide_text}>
+                            <a className="slideLink" href={slideLinks[3]}>{slideTitles[3]}</a>
+                        </div>
+                    </div>
+                    <div className="slide_image" style={{'backgroundImage': `url(${slideImages[3]})`, 'height': 'calc(100vh - 50px)'}}>
                     </div>
                 </div>
             </Slide>
         </div>
     )
+}
+
+const slide_text = {
+
 }
 
 export default ImageSlider 
